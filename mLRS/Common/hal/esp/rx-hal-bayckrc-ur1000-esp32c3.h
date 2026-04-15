4a1b75b3fc02abf35559d4dd570ed1ce60d274ea
Amigache <amigache@hotmail.com>
2026-04-15 09:28:39 +0200

    esp, add support for BAYCKRC UR1000 receiver with configuration and HAL files

---
+++ mLRS/Common/hal/esp/rx-hal-bayckrc-ur1000-esp32c3.h
@@ -0,0 +1,176 @@
+//*******************************************************
+// Copyright (c) MLRS project
+// GPL3
+// https://www.gnu.org/licenses/gpl-3.0.de.html
+//*******************************************************
+// hal
+//********************************************************
+
+//-------------------------------------------------------
+// ESP32C3, ELRS BAYCKRC UR1000 Dual Band RX
+//-------------------------------------------------------
+
+#define DEVICE_HAS_SINGLE_LED_RGB
+#define DEVICE_HAS_OUT
+#define DEVICE_HAS_NO_DEBUG
+//#define DEVICE_HAS_SERIAL_OR_DEBUG
+
+
+//-- UARTS
+// UARTB = serial port
+// UART  = output port, SBus or whatever
+// UARTF = debug port
+
+#define UARTB_USE_SERIAL
+#define UARTB_BAUD                RX_SERIAL_BAUDRATE
+#define UARTB_USE_TX_IO           IO_P21
+#define UARTB_USE_RX_IO           IO_P20
+#define UARTB_TXBUFSIZE           RX_SERIAL_TXBUFSIZE
+#define UARTB_RXBUFSIZE           RX_SERIAL_RXBUFSIZE
+
+#define UART_USE_SERIAL1
+#define UART_BAUD                 416666   // CRSF baud rate
+#define UART_USE_TX_IO            IO_P19   // tx2 pad on the receiver
+#define UART_USE_RX_IO            -1       // no Rx pin needed
+#define UART_TXBUFSIZE            256
+
+#define UARTF_USE_SERIAL
+#define UARTF_BAUD                115200
+
+
+//-- SX1: LR11xx & SPI
+
+#define SPI_CS_IO                 IO_P7
+#define SPI_MISO                  IO_P5
+#define SPI_MOSI                  IO_P4
+#define SPI_SCK                   IO_P6
+#define SPI_FREQUENCY             16000000L  // 16 MHz max per datasheet
+#define SX_BUSY                   IO_P3
+#define SX_DIO1                   IO_P1
+#define SX_RESET                  IO_P2
+
+#define SX_USE_REGULATOR_MODE_DCDC
+
+IRQHANDLER(void SX_DIO_EXTI_IRQHandler(void);)
+
+void sx_init_gpio(void)
+{
+    gpio_init(SX_DIO1, IO_MODE_INPUT_ANALOG);
+    gpio_init(SX_BUSY, IO_MODE_INPUT_ANALOG);
+    gpio_init(SX_RESET, IO_MODE_OUTPUT_PP_LOW);
+}
+
+IRAM_ATTR bool sx_busy_read(void)
+{
+    return (gpio_read_activehigh(SX_BUSY)) ? true : false;
+}
+
+IRAM_ATTR void sx_amp_transmit(void) {}
+
+IRAM_ATTR void sx_amp_receive(void) {}
+
+void sx_dio_init_exti_isroff(void)
+{
+    detachInterrupt(SX_DIO1);
+}
+
+void sx_dio_enable_exti_isr(void)
+{
+    attachInterrupt(SX_DIO1, SX_DIO_EXTI_IRQHandler, RISING);
+}
+
+IRAM_ATTR void sx_dio_exti_isr_clearflag(void) {}
+
+
+//-- Out port
+
+void out_init_gpio(void) {}
+
+void out_set_normal(void)
+{
+    // https://github.com/espressif/esp-idf/blob/release/v4.4/components/esp_rom/include/esp32c3/rom/gpio.h#L236
+    gpio_matrix_out((gpio_num_t)UART_USE_TX_IO, U1TXD_OUT_IDX, false, false);
+}
+
+void out_set_inverted(void)
+{
+    gpio_matrix_out((gpio_num_t)UART_USE_TX_IO, U1TXD_OUT_IDX, true, false);
+}
+
+
+//-- Button
+
+#define BUTTON                    IO_P9
+
+void button_init(void)
+{
+    gpio_init(BUTTON, IO_MODE_INPUT_PU);
+}
+
+IRAM_ATTR bool button_pressed(void)
+{
+    return gpio_read_activelow(BUTTON) ? true : false;
+}
+
+
+//-- LEDs
+
+#define LED_RGB                   IO_P8
+#define LED_RGB_PIXEL_NUM         1
+#include "../esp-hal-led-rgb.h"
+
+
+//-- LR11xx DIO switch control
+
+#define SX_USE_RFSW_CTRL          {15, 0, 4, 12, 0, 2, 0, 1}
+
+
+//-- POWER
+
+#include "../../setup_types.h" // needed for frequency band condition in rfpower calc
+
+#define SX_USE_LP_PA  // radio_rfo_hf:true in ELRS = LP PA for sub-GHz
+
+void lr11xx_rfpower_calc(const int8_t power_dbm, int8_t* sx_power, int8_t* actual_power_dbm, const uint8_t frequency_band)
+{
+    if (frequency_band == SX_FHSS_FREQUENCY_BAND_2P4_GHZ) {
+        // HF PA, power_values_dual: [-9, -4, -1, 3]
+        if (power_dbm >= POWER_3_DBM) {
+            *sx_power = 3;
+            *actual_power_dbm = 3;
+        } else if (power_dbm >= -1) {
+            *sx_power = -1;
+            *actual_power_dbm = -1;
+        } else if (power_dbm >= -4) {
+            *sx_power = -4;
+            *actual_power_dbm = -4;
+        } else {
+            *sx_power = -9;
+            *actual_power_dbm = -9;
+        }
+    } else {
+        // LP PA (radio_rfo_hf:true), power_values: [-8, -4, -1, 3]
+        if (power_dbm >= POWER_3_DBM) {
+            *sx_power = 3;
+            *actual_power_dbm = 3;
+        } else if (power_dbm >= -1) {
+            *sx_power = -1;
+            *actual_power_dbm = -1;
+        } else if (power_dbm >= -4) {
+            *sx_power = -4;
+            *actual_power_dbm = -4;
+        } else {
+            *sx_power = -8;
+            *actual_power_dbm = -8;
+        }
+    }
+}
+
+#define RFPOWER_DEFAULT           3 // index into rfpower_list array
+
+const rfpower_t rfpower_list[] = {
+    { .dbm = POWER_m9_DBM, .mW = 0 },
+    { .dbm = -4, .mW = 0 },
+    { .dbm = -1, .mW = 1 },
+    { .dbm = POWER_3_DBM, .mW = 2 },
+};
